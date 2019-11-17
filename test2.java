0c7163300898eed4a6a9536e47d7db1e2398ab2c
pratik-suraywanshi <pratik15121992@gmail.com>
2019-11-17 11:23:38 +0530

    modified test2.java again

--- test2.java
+++ test2.java
@@ -2,5 +2,7 @@ public class test2 {
     public static void main(String args[]) {
         System.out.println("HI I am test2");
         System.out.println("just check);
+                System.out.println("just check);
+
     }
 }
